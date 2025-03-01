7fd64b91de5961e528619f9376a880c70763cccf
河西玄太 <kasai@kasaigenfutoshinoMacBook-Air.local>
2025-03-01 17:03:49 +0900

    Add: appフォルダのルーティング

---
+++ app/blog/page.js
@@ -0,0 +1,6 @@
+const Blog = () => {
+    return (
+        <h1>Blog page.</h1>
+    )
+}
+export default Blog;
--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 
-export default function Home() {
+const Index = () =>{
+  return (
+    <h1>Hello.</h1>
+  );
 }
+export default Index;
