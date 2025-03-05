c02229750d3d29fb5e2785c27580350d56e5343b
河西玄太 <kasai@kasaigenfutoshinoMacBook-Air.local>
2025-03-05 23:25:28 +0900

    Update: 骨組みを作成

---
+++ app/components/header.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import Link from "next/link";
+import logoImage from "../../public/images/logo.png";
+
+const Header =()=>{
+    return(
+        <>
+        <header>
+            <div>
+                <div>
+                    <Link href="/">
+                    <Image src={logoImage} alt="logo"></Image>
+                    </Link>
+                    <ul>
+                        <li><Link href="/blog">Blog</Link></li>
+                        <li><Link href="/contact">Contact</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </header>
+        </>
+    )
+}
+export default Header;
