ae0509ea52c7059a45433f1a099865be770c7205
河西玄太 <kasai@kasaigenfutoshinoMacBook-Air.local>
2025-03-06 13:54:55 +0900

    Update: すべてのマークダウンファイルを取得する関数(getAllBlogs)とひとつだけマークダウンファイルを取得する関数(getSingleBlog)を共有化

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,19 +1,17 @@
-import fs from 'fs'; //ファイルシステムを操作するためのモジュール
-import path from 'path';
-import matter from 'gray-matter'; //ファイルの内容を解析するためのモジュール
 import ReactMarkdown from 'react-markdown';
 import Image from 'next/image';
+import { getAllBlogs,getSingleBlog } from '@/app/utils/mdQueries';
+import PrevNext from "../../components/prevNext";
 
-async function getSingleBlog(context) {
-    const { slug } = await context.params;
-    const data = await import(`../../../data/${slug}.md`)
-    const singleDocument = matter(data.default)
-    return {
-        singleDocument: singleDocument
-    }
-}
 const SingleBlog = async(props) =>{
     const {singleDocument} = await getSingleBlog(props);
+    const {blogs} = await getAllBlogs();
+    // 次のページ
+    const prev = blogs.filter(blog => blog.frontmatter.id === 
+        singleDocument.data.id -1)
+    // 戻るページ
+    const next = blogs.filter(blog => blog.frontmatter.id === 
+        singleDocument.data.id + 1)
     return (
         <>
         <div className='img-container'>
@@ -24,32 +22,14 @@ const SingleBlog = async(props) =>{
                 <h1>{singleDocument.data.title}</h1>
                 <p>{singleDocument.data.date}</p> 
                 <ReactMarkdown>{singleDocument.content}</ReactMarkdown>
-            </div>               
+            </div>
+            <PrevNext prev={prev} next={next}/>
         </div>
         </>
     )
 }
 export default SingleBlog;
 export async function generateStaticParams() {
-    async function getAllBlogs(){
-        // fs.readdirSync() 指定したディレクトリ内のファイル名を取得する関数
-        const files = fs.readdirSync(path.join("data")); //process.cwd(); カレントディレクトリ(Curent Working Directory)を取得
-        const blogs = files.map((fileName) => {
-            const slug = fileName.replace(".md",""); //ファイル名から拡張子を削除
-            const fileData = fs.readFileSync(
-                path.join("data",fileName),
-                "utf-8"
-            );
-            const { data } = matter(fileData) 
-            return {
-                frontmatter: data,
-                slug: slug 
-            }
-        });
-        return{
-            blogs:blogs
-        }
-    }
     const {blogs} = await getAllBlogs();
     const paths = blogs.map((blog) => ({
         params: { slug: blog.slug }
